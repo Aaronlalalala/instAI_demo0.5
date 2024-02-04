ae40ca4853989e485226070c2610982359c9894b
Aaronlalalala <9268michael@gmail.com>
2024-02-04 09:53:36 +0800

    2/4錯誤維修

--- src/src/Nevigation/Model/Model.js
+++ src/src/Nevigation/Model/Model.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { NavLink, useLocation } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 import InstAI_icon from "../../image/instai_icon.png";
 
 
@@ -48,7 +48,6 @@ const Model = () => {
     }
   };
 
-
   return (
     <div className="container-fluid mt-3">
       <div className="row d-flex justify-content-between ">
@@ -65,34 +64,26 @@ const Model = () => {
       <h1 className='main-projectTitle'>
         {projectname}
       </h1>
-      <div className='background' style={{ position: 'relative' }}>
-        
+      <div className='background' style={{ position: 'relative' }}>    
         <img src={bell} alt="bell" style={{ width: '150px', height: '150px' ,marginRight: '120px',marginLeft:'120px' }}></img>
         <img src={schedule} alt="schedule" style={{ width: '150px', height: '150px',marginRight: '120px' }}></img>
         <img src={design} alt="design" style={{ width: '150px', height: '150px',marginRight: '120px' }}></img>
         <img src={train} alt="train" style={{ width: '150px', height: '150px',marginRight: '120px' }}></img>
         <img src={line} alt="line" style={{ width: '150px', height: '150px' }}></img>
-
         <div style={{ display: 'flex', marginTop: '20px' }}>
           <div style={{ width: '20px', height: '20px', borderRadius: '50%', background: 'white', border: '2px solid #000', marginRight: '250px' ,marginLeft: '190px'}}></div>
           <div style={{ width: '20px', height: '20px', borderRadius: '50%', background: 'white', border: '2px solid #000', marginRight: '250px' }}></div>
           <div style={{ width: '20px', height: '20px', borderRadius: '50%', background: 'white', border: '2px solid #000', marginRight: '250px' }}></div>
           <div style={{ width: '20px', height: '20px', borderRadius: '50%', background: 'white', border: '2px solid #000', marginRight: '250px' }}></div>
-          <div style={{ width: '20px', height: '20px', borderRadius: '50%', background: 'white', border: '2px solid #000' }}></div>
-          
+          <div style={{ width: '20px', height: '20px', borderRadius: '50%', background: 'white', border: '2px solid #000' }}></div>         
         </div>
-
         <div style={{ position: 'absolute', left: '50%',marginTop:'-10px', transform: 'translateX(-50%)', width: '100%', borderBottom: '2px solid #000' }}></div>
         <div className="col mt-3">
         <ul>
           <button className='listTitle' style={{marginLeft:'600px',marginTop:'200px'}} onClick={handleDownloadModel}>Download AI model</button>
         </ul>
-        <ul>我也不知道這是不是模型檔案
-        </ul>
       </div>
-      </div>
-
-      
+      </div>      
     </div>
   );
 }
